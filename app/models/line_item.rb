d839bf9810cc06d11a201333bc426debb671cf9d
arkadiyk <arkadiyk@gmail.com>
2008-12-03 02:09:37 +0000

    progress

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -9,13 +9,18 @@ class LineItem < ActiveRecord::Base
   def supplier
     catalog_item.supplier
   end
+  def self.find_all_by_supplier_id( supplier_id )
+    find :all, :include => :catalog_item, :conditions => ["catalog_items.supplier_id = ?", supplier_id]
+  end
   
   # belongs_to :product_line, :through => :catalog_items
   def product_line
     catalog_item.product_line
   end
   
-  private    
+  
+  
+  private       
     def update_fields_from_catalog_item
       return if self.catalog_item.nil? 
       self.price = self.catalog_item.price
