f9e1d44590e7ef6a7dcc08250dbb67eb73345db5
arkadiyk <arkadiyk@gmail.com>
2008-12-13 22:37:42 +0000

    add Cap deployment descripter

---
+++ config/deploy.rb
@@ -0,0 +1,54 @@
+#############################################################
+## General
+#############################################################
+set :application, "tento"
+
+
+#############################################################
+## Servers 
+#############################################################
+set :use_sudo, false    # Dreamhost does not support sudo
+set :user, "tento"      # Dreamhost SSH User
+set :domain, "tentoexpress.jp"
+
+server domain, :app, :web
+role :db, domain, :primary => true
+
+
+#############################################################
+## Subversion
+#############################################################
+set :scm, :subversion
+set :scm_user, "arc"   # Sets 'my_svn_user' instead, if you are using different name than your app.
+set :scm_auth_cache, true  # Prompts for password once
+set :scm_password, "arc123" #Proc.new { Capistrano::CLI.password_prompt("SCM password for #{scm_user}:") }
+set :repository,  "http://svn.tentoexpress.jp/#{application}2/trunk"
+set :deploy_to, "/home/#{user}/#{domain}"
+# keeps a local checkout of the repository on the server to get faster deployments
+set :deploy_via, :remote_cache
+
+
+#############################################################
+## Tasks
+#############################################################
+
+namespace :deploy do
+  desc "Restart Application (using tmp/restart.txt)"
+  task :restart_passenger do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Restarts your application."
+  task :restart do
+    restart_passenger
+  end
+
+  desc "Link shared files"
+  #task :before_symlink do
+  before :symlink do
+    run "rm -drf #{release_path}/public/uploads"
+    run "ln -s #{shared_path}/uploads #{release_path}/public/uploads"
+    run "rm -f #{release_path}/config/database.yml"
+    run "ln -s #{shared_path}/database.yml #{release_path}/config/database.yml"
+  end
+end
