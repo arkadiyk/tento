e482f21c2bba35173584ad681ba5cb84862806ab
arkadiyk <arkadiyk@gmail.com>
2009-02-28 12:54:31 +0000

    progress

--- app/controllers/customer_orders_controller.rb
+++ app/controllers/customer_orders_controller.rb
@@ -1,13 +1,22 @@
 class CustomerOrdersController < ApplicationController
-  def show
+  def show    
   end
 
   def index
+    @orders = user_orders
     if params[:p1]
+      @corder = Cart.find_by_order_id current_user, params[:p1]
+      if @corder
+        idx = @orders.index(@corder)
+        if idx > 0
+          @prev = @orders[idx - 1].order_id
+        end
+        if idx < @orders.length - 1
+          @next = @orders[idx + 1].order_id
+        end
+      end
       render :action => "show"
     end
-    
-    @orders = user_orders
   end
 
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,6 @@
 class Cart < ActiveRecord::Base
+  ORDER_INIT_NUM = 77100
+  
   has_many :orders
   has_many :line_items, :through => :orders
 
@@ -27,7 +29,16 @@ class Cart < ActiveRecord::Base
   end
   
   def order_id
-    "#{pay_method}00#{id + 77100}" 
+    "#{pay_method}00#{id + ORDER_INIT_NUM}" 
+  end
+  
+  def updatable?
+    orders.max(:status) < 2
+  end
+  
+  def self.find_by_order_id(user, order_id)
+    oid = order_id.sub(/^../,'').to_i - ORDER_INIT_NUM
+    find :first, :conditions => {:id => oid, :user_id => user}
   end
   
   def title
